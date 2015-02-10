a10bc31d85ab7a1387422faf227a28391636e59c
Justin Khan <jkhan@samsaodev.com>
2015-02-10 14:11:57 -0500

    Created a Facebook login button

---
+++ Wink/Helpers/FacebookHelper.m
@@ -0,0 +1,13 @@
+//
+//  FacebookHelper.m
+//  Wink
+//
+//  Created by Justin Khan on 2015-02-10.
+//  Copyright (c) 2015 Samsao. All rights reserved.
+//
+
+#import "FacebookHelper.h"
+
+@implementation FacebookHelper
+
+@end
