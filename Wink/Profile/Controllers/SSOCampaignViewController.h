35ae620cf33290edbb7e692f780d49bd3db0016d
Marcelo de Souza <mdesouza@samsaodev.com>
2015-04-29 14:53:51 -0400

    Profile Page * Starting the creation of the My feeds

---
+++ Wink/Profile/Controllers/SSOCampaignViewController.h
@@ -0,0 +1,15 @@
+//
+//  SSOCampaignViewController.h
+//  uSnap
+//
+//  Created by Marcelo De Souza on 2015-04-28.
+//  Copyright (c) 2015 Samsao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSOCampaignViewController : UIViewController
+
+- (void)setCampaignData:(NSArray *)data;
+
+@end
