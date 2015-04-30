4beaa89523903e7ad0a45ace3622828e4d05b243
Gabriel Cartier <gcartier@samsaodev.com>
2015-04-30 14:13:21 -0400

    Started working on the photo detail view

---
+++ Wink/Photos/Controllers/SSOPhotoDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSOPhotoDetailViewController.h
+//  uSnap
+//
+//  Created by Gabriel Cartier on 2015-04-30.
+//  Copyright (c) 2015 Samsao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSOPhotoDetailViewController : UIViewController
+
+@end
---
+++ Wink/Photos/Controllers/SSOPhotoDetailViewController.m
@@ -0,0 +1,41 @@
+//
+//  SSOPhotoDetailViewController.m
+//  uSnap
+//
+//  Created by Gabriel Cartier on 2015-04-30.
+//  Copyright (c) 2015 Samsao. All rights reserved.
+//
+
+#import "SSOPhotoDetailViewController.h"
+
+@interface SSOPhotoDetailViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (weak, nonatomic) IBOutlet UIView *social8ss69;
+
+@end
+
+@implementation SSOPhotoDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
