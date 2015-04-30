b5efb4c525c446b3158eae8ec6dc11ae1c567494
Nicolas Vincensini <nvincensini@samsaodev.com>
2015-04-30 18:09:23 -0400

    Added username property to SSOSnap

--- Wink/Photos/Model/SSOSnap.h
+++ Wink/Photos/Model/SSOSnap.h
@@ -2,22 +2,23 @@
 
 @interface SSOSnap : NSObject
 
-@property (nonatomic, strong) NSString * appId;
-@property (nonatomic, strong) NSString * email;
-@property (nonatomic, strong) NSString * fb;
-@property (nonatomic, strong) NSObject * fbImageId;
-@property (nonatomic, strong) NSNumber * fbLikes;
-@property (nonatomic, strong) NSString * filename;
-@property (nonatomic, strong) NSObject * gp;
-@property (nonatomic, strong) NSString * id;
-@property (nonatomic, strong) NSString * imageData;
-@property (nonatomic, strong) NSString * meta;
-@property (nonatomic, strong) NSString * status;
-@property (nonatomic, strong) NSObject * text;
-@property (nonatomic, strong) NSObject * tw;
-@property (nonatomic, strong) NSString * url;
+@property(nonatomic, strong) NSString *appId;
+@property(nonatomic, strong) NSString *username;
+@property(nonatomic, strong) NSString *email;
+@property(nonatomic, strong) NSString *fb;
+@property(nonatomic, strong) NSObject *fbImageId;
+@property(nonatomic, strong) NSNumber *fbLikes;
+@property(nonatomic, strong) NSString *filename;
+@property(nonatomic, strong) NSObject *gp;
+@property(nonatomic, strong) NSString *id;
+@property(nonatomic, strong) NSString *imageData;
+@property(nonatomic, strong) NSString *meta;
+@property(nonatomic, strong) NSString *status;
+@property(nonatomic, strong) NSObject *text;
+@property(nonatomic, strong) NSObject *tw;
+@property(nonatomic, strong) NSString *url;
 
--(instancetype)initWithDictionary:(NSDictionary *)dictionary;
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
--(NSDictionary *)toDictionary;
+- (NSDictionary *)toDictionary;
 @end
