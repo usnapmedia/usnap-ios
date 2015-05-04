e674f99ad37934ef709a020d90e8a5a9722c9524
Nicolas Vincensini <nvincensini@samsaodev.com>
2015-05-04 19:38:18 -0400

    Changed assets

---
+++ Wink/Categories/UIButton+VerticalLayout.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+VerticalLayout.h
+//  uSnap
+//
+//  Created by Nicolas Vincensini on 2015-05-04.
+//  Copyright (c) 2015 Samsao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (VerticalLayout)
+
+@end
---
+++ Wink/Categories/UIButton+VerticalLayout.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+VerticalLayout.m
+//  uSnap
+//
+//  Created by Nicolas Vincensini on 2015-05-04.
+//  Copyright (c) 2015 Samsao. All rights reserved.
+//
+
+#import "UIButton+VerticalLayout.h"
+
+@implementation UIButton (VerticalLayout)
+
+@end
