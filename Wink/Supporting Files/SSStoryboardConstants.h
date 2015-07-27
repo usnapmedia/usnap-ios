2182e3efe4c493b488664d12ae14b1e16a974c6f
Gabriel Cartier <gcartier@samsaodev.com>
2015-07-27 11:20:07 -0400

    Removed useless constants

--- Wink/Supporting Files/SSStoryboardConstants.h
+++
@@ -1,7 +0,0 @@
-/* Generated document. DO NOT CHANGE */
-
-/* Segue identifier constants */
-
-/* Controller identifier constants */
-extern NSString * const CAMERA_NAV_VC;
-extern NSString * const CAMERA_VC;
--- Wink/Supporting Files/SSStoryboardConstants.m
+++
@@ -1,5 +0,0 @@
-/* Generated document. DO NOT CHANGE */
-
-
-NSString * const CAMERA_NAV_VC = @"CAMERA_NAV_VC";
-NSString * const CAMERA_VC = @"CAMERA_VC";
